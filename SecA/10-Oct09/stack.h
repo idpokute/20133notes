2101b25f7fb32d20052d12c1d145c4f5e7432b1c
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2013-10-09 12:29:45 -0400

    Stack sec A

---
+++ SecA/10-Oct09/stack.h
@@ -0,0 +1,26 @@
+#ifndef __FS_STACK_H__
+#define __FS_STACK_H__
+namespace oop344{
+  class Stack;
+
+  class Node{
+    int _data;
+    Node* _next;
+    Node(int data, Node* next = (Node*)0);
+    friend class Stack;
+    virtual ~Node();
+  };
+
+
+  class Stack{
+    Node* _top;
+  public:
+    Stack();
+    void push(int data);
+    int pop();
+    bool isEmpty();
+    virtual ~Stack();
+  };
+
+}
+#endif
