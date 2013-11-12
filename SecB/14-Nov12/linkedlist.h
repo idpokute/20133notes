d63bc311e1e4354e8d6c58e3152b0d8ceecf63e5
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2013-11-12 11:52:28 -0500

    final nov12 SecB Changes

--- SecB/14-Nov12/linkedlist.h
+++ SecB/14-Nov12/linkedlist.h
@@ -17,53 +17,53 @@ namespace oop344{
    
   template <class T>
   class LinkedList{
-      Node<T>* _head;
-      Node<T>* _tail;
+    Node<T>* _head;
+    Node<T>* _tail;
   public:
-      LinkedList();
-      virtual ~LinkedList();
-      void add(T);
-      void push(T);
-      T remove();
-      T pop();
-      T operator[](int index); // if index is passed the size of the list
-                               // make nodes with default data to accomodate it ...
-                               // hense T needs to be able to be defaulted
-      bool isEmpty();
+    LinkedList();
+    virtual ~LinkedList();
+    void add(T);
+    void push(T);
+    T remove();
+    T pop();
+    T& operator[](int index); // if index is passed the size of the list
+                              // make nodes with default data to accomodate it ...
+                              // hense T needs to be able to be defaulted
+    bool isEmpty();
   };
   template <class T>
   Node<T>::Node(T data, Node<T>* prev, Node<T>* next):_data(data){
-      _prev = prev;
-      _next = next;
+    _prev = prev;
+    _next = next;
   }
   template <class T>
   Node<T>::~Node(){
   }
   template <class T>
   LinkedList<T>::LinkedList(){
-      _head = _tail = (Node<T>*)0;
+    _head = _tail = (Node<T>*)0;
   }
    
   template <class T>
   LinkedList<T>::~LinkedList(){
-      Node<T>* toDel;
-      while(toDel = _head){            //While toDel is equal to _head
-          _head = _head -> _next;        //Make head point to the next
-          delete toDel;                //Delete the toDel
-      }
+    Node<T>* toDel;
+    while(toDel = _head){            //While toDel is equal to _head
+      _head = _head -> _next;        //Make head point to the next
+      delete toDel;                //Delete the toDel
+    }
   }
    
   template <class T>
   void LinkedList<T>::add(T data){
-      Node<T>* NN = new Node<T>(data, _tail);    //Create a NN(NewNode) to start at the tail
-      if (!isEmpty()) {
-          _tail->_next = NN;                    //Then point the tail to _next which now equals NN
-          //You can also do "NN->_prev->_next = NN" crazy version of above
-          _tail = NN; //OR _tail = _tail -> _next;
-      }
-      else {
-          _tail = _head = NN;
-      }
+    Node<T>* NN = new Node<T>(data, _tail);    //Create a NN(NewNode) to start at the tail
+    if (!isEmpty()) {
+      _tail->_next = NN;                    //Then point the tail to _next which now equals NN
+      //You can also do "NN->_prev->_next = NN" crazy version of above
+      _tail = NN; //OR _tail = _tail -> _next;
+    }
+    else {
+      _tail = _head = NN;
+    }
   }
 
   template <class T>
@@ -82,26 +82,29 @@ namespace oop344{
   T LinkedList<T>::pop(){
     return remove();
   }
-   
+  template <class T>
+  T& LinkedList<T>::operator[](int index){
+    return _head->_data;
+  }
   template <class T>
   T LinkedList<T>::remove(){
-      T ret(_head->_data);
-      //auto toRem = _head; do this on test, not in a program, it automatically chooses Node*
-      Node<T>* toRem = _head;
-      _head = _head->_next;
-      if(_head){                    //Not Empty
-          _head->_prev = (Node<T>*)0; //Obsolete if head is equal to null
-      }
-      else {
-          _tail = (Node<T>*)0;
-      }
-      delete toRem;
-      return ret;
+    T ret(_head->_data);
+    //auto toRem = _head; do this on test, not in a program, it automatically chooses Node*
+    Node<T>* toRem = _head;
+    _head = _head->_next;
+    if(_head){                    //Not Empty
+      _head->_prev = (Node<T>*)0; //Obsolete if head is equal to null
+    }
+    else {
+      _tail = (Node<T>*)0;
+    }
+    delete toRem;
+    return ret;
   }
 
   template <class T>
   bool LinkedList<T>::isEmpty(){
-      return !_head;
+    return !_head;
   }
 }
 #endif
