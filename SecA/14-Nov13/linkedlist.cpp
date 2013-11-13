3778db53b3392f8b0da2905fc6ec4098df4dc1b8
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2013-11-13 12:05:32 -0500

    more on Nov 13 SecA

--- SecA/14-Nov13/linkedlist.cpp
+++ SecA/14-Nov13/linkedlist.cpp
@@ -1,34 +1,59 @@
 #include "linkedlist.h"
 namespace oop344{
-  Node::Node(int data, Node* prev, Node* next){
-  
+  Node::Node(int data, 
+    Node* prev, Node* next){
+    _data = data;
+    _prev = prev;
+    _next = next;
   }
  
   Node::~Node(){
   }
- 
-  LinkedList::LinkedList(){
   
+  template <class T>
+  LinkedList<T>::LinkedList(){
+    _head = _tail = (Node*)0 ;
   }
    
  
   LinkedList::~LinkedList(){
- 
+    Node* toDel;
+    while(toDel = _head ){
+      _head = _head->_next;
+      delete toDel;
+    }
   }
    
  
   void LinkedList::add(int data){
- 
+    Node* NN = new Node(data, _tail, (Node*)0);
+    if(!isEmpty()){
+      _tail->_next = NN;
+      _tail = NN;
+    }
+    else{
+      _head = _tail = NN;
+    }
   }
 
  
  
   int LinkedList::remove(){
- 
+    int ret = _head->_data;
+    Node* toRem = _head;
+    _head = _head->_next;
+    if(!isEmpty()){
+      _head->_prev = (Node*)0;
+    }
+    else{
+      _tail = (Node*)0;
+    }
+    delete toRem;
+    return ret;
   }
 
  
   bool LinkedList::isEmpty(){
- 
+    return !_head;
   }
 }
--- SecA/14-Nov13/linkedlist.h
+++ SecA/14-Nov13/linkedlist.h
@@ -1,21 +1,32 @@
 #ifndef __344_LINKEDLIST_
 #define __344_LINKEDLIST_
+/*
+1 - add the template command to every and each scope
+    "template <class T>"
+2 - replace all reletive type in the template scope to "T"
+3 - add the sign. of template ("<T>") to every and each class reletive to the template 
+   exp: (do not add to the following)
+      1 - the class name right after template command
+      2 - Constructor names
+      3 - Distructor names
+4 - Move all the code to the header file (templates can't exist in cpp files)
+5 - document all the special needs for the templated type.
+*/
 
 namespace oop344{
-    
+
   class LinkedList;
-   
- 
+  template <class T>
   class Node{
-    Node* _next;
-    Node* _prev;
-    int _data;
-    Node(int data, Node* prev, Node* next);
+    Node<T>* _next;
+    Node<T>* _prev;
+    T _data;
+    Node(T data, 
+      Node<T>* prev, Node<T>* next);
     virtual ~Node();
-    friend class LinkedList;
+    friend class LinkedList<T>;
   };
-   
- 
+
   class LinkedList{
     Node* _head;
     Node* _tail;
@@ -23,7 +34,7 @@ namespace oop344{
     LinkedList();
     virtual ~LinkedList();
     int remove();
-    int pop();
+    void add(int);
     bool isEmpty();
   };
 }
