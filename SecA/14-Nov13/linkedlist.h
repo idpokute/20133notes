bb6ee0e927bdcf425cf8b6f042eb2385ef0cdca5
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2013-11-13 12:40:30 -0500

    Nov 13 SecA

--- SecA/14-Nov13/linkedlist.h
+++ SecA/14-Nov13/linkedlist.h
@@ -14,7 +14,7 @@
 */
 
 namespace oop344{
-
+  template <class T>
   class LinkedList;
   template <class T>
   class Node{
@@ -26,16 +26,83 @@ namespace oop344{
     virtual ~Node();
     friend class LinkedList<T>;
   };
-
+  template <class T>
   class LinkedList{
-    Node* _head;
-    Node* _tail;
+    Node<T>* _head;
+    Node<T>* _tail;
   public:
     LinkedList();
     virtual ~LinkedList();
-    int remove();
-    void add(int);
+    void push(T);
+    T pop();
+    T remove(); 
+    void add(T);
     bool isEmpty();
   };
+  template <class T>
+  Node<T>::Node(T data, 
+      Node<T>* prev, Node<T>* next){
+    _data = data;
+    _prev = prev;
+    _next = next;
+  }
+ 
+  template <class T>
+  Node<T>::~Node(){
+  }
+  
+  template <class T>
+  LinkedList<T>::LinkedList(){
+    _head = _tail = (Node<T>*)0 ;
+  }
+   
+ 
+  template <class T>
+  LinkedList<T>::~LinkedList(){
+    Node<T>* toDel;
+    while(toDel = _head ){
+      _head = _head->_next;
+      delete toDel;
+    }
+  }
+   
+ 
+  template <class T>
+  void LinkedList<T>::add(T data){
+    Node<T>* NN = new Node<T>(data, _tail, (Node<T>*)0);
+    if(!isEmpty()){
+      _tail->_next = NN;
+      _tail = NN;
+    }
+    else{
+      _head = _tail = NN;
+    }
+  }
+
+  template <class T>
+  T LinkedList<T>::pop(){
+    return remove();
+  }
+ 
+  template <class T>
+  T LinkedList<T>::remove(){
+    T ret = _head->_data;
+    Node<T>* toRem = _head;
+    _head = _head->_next;
+    if(!isEmpty()){
+      _head->_prev = (Node<T>*)0;
+    }
+    else{
+      _tail = (Node<T>*)0;
+    }
+    delete toRem;
+    return ret;
+  }
+
+ 
+  template <class T>
+  bool LinkedList<T>::isEmpty(){
+    return !_head;
+  }
 }
 #endif
