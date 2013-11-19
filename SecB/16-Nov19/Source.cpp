4e1f0ec3edc160341f965255778368d69ed4f3c6
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2013-11-19 11:37:12 -0500

    nov 19

--- SecB/16-Nov19/Source.cpp
+++ SecB/16-Nov19/Source.cpp
@@ -1,41 +1,35 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
-//bool isOn(unsigned int val,unsigned int n){
-//  unsigned int mask = 1 << (n-1);
-//  bool ret = false;
-//  if((val & mask) != 0){
-//    ret = true;
-//  }
-//  return ret;
-//}
-//bool isOn(unsigned int val,unsigned int n){
-//  unsigned int mask = 1 << (n-1);
-//  return (val & mask) != 0;
-//}
-//bool isOn(unsigned int val,unsigned int n){
-//  return (val & (1 << (n-1))) != 0;
-//}
+
 bool isOn(unsigned int val,unsigned int n){
   return bool(val & (1 << (n-1)));
 }
 
-//void prnBits(unsigned int val){
-//  int i = sizeof(int)*8;
-//  do{
-//    cout<<isOn(val, i);
-//  }while(--i);
-//}
-//void prnBits(unsigned int val){
-//  for(int i=sizeof(int)*8;i--;cout<<bool(val & 1<<i));
-//}
-//void prnBits(unsigned int val){
-//  for(int i=sizeof(int)*8;i--;cout<<(val & 1<<i)?1:0);
-//}
+
 void prnBits(unsigned int val){
   for(int i=sizeof(int)*8;i--;cout<<!!(val & 1<<i));
 }
-int main(){
-  prnBits(0xf0f0f0f0);
+//const char* bits(unsigned int val){
+//  static char bitstr[512];
+//  int j = 0;
+//  for(int i=sizeof(int)*8;i--;bitstr[j++] = !!(val & 1<<i)?'1':'0');
+//  bitstr[j] = 0;
+//  return bitstr;
+//}
 
+enum boolean{False, True};
+const char* bits(unsigned int val){
+  static char bitstr[512];
+  int j = 0;
+  for(int i=sizeof(int)*8;i--;bitstr[j++] = !!(val & 1<<i)+'0');
+  bitstr[j] = 0;
+  return bitstr;
+}
+int main(){
+  
+  unsigned int a = 0xf0f0f0f0;
+  cout<<bits(a)<<endl;
+  printf("%s\n", bits(a));
   return 0;
 }
